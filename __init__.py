952dc4ad0dcdd4a694bb7e90fc53d916459c905d
Janzel25 <j.claudio@cfacampusmontsouris.fr>
2026-02-18 15:29:19 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,8 @@
 from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
-from flask import render_template
-from flask import json
 from urllib.request import urlopen
 from werkzeug.utils import secure_filename
 import sqlite3
 
-
-# app.py
-from flask import Flask, request, jsonify
-import sqlite3
 
 
 app = Flask(__name__)                                                                                                                  
